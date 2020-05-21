89fdca4420efcc8fdb8d20a9f6ce791c0e4b1d0c
HonmaMeiko <17mwang2@stu.edu.cn>
2020-05-21 20:18:39 +0800

    use Hook API and functional component to overwrite previous class component

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,23 +1,3 @@
-import React from 'react';
+/// <reference types="react" />
 import './common/css/List.css';
-interface LatestData {
-    population: string;
-    total_case: string;
-    total_death: string;
-}
-interface MergeCountryStatistics {
-    [countryName: string]: Array<LatestData>;
-}
-interface Props {
-}
-interface State {
-    data: MergeCountryStatistics;
-}
-export default class List extends React.Component<Props, State> {
-    private latestWorldStatistics;
-    constructor(props: Props);
-    private listAllData;
-    componentDidMount(): void;
-    render(): JSX.Element;
-}
-export {};
+export default function List(): JSX.Element;
--- src/index.tsx
+++ src/index.tsx
@@ -1,63 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './common/css/List.css'
-import {getData} from './common/utils/getCovidData'
-interface LatestData{
-    population:string
-    total_case:string
-    total_death:string
+import { getData } from './common/utils/getCovidData'
+interface LatestData {
+  population: string
+  total_case: string
+  total_death: string
 }
-interface MergeCountryStatistics{
-  [countryName:string]:Array<LatestData>
+interface MergeCountryStatistics {
+  [countryName: string]: Array<LatestData>
 }
-interface Props{
-}
-interface State{
-  data:MergeCountryStatistics
-}
-export default class List extends React.Component<Props,State>{
-  private latestWorldStatistics:Array<JSX.Element>=[]
-  constructor(props:Props){
-    super(props)
-    this.state={data:{}}
-    this.listAllData=this.listAllData.bind(this)
-  }
-  private listAllData(data:MergeCountryStatistics):Array<JSX.Element>{
-    if(this.latestWorldStatistics.length!==0) return this.latestWorldStatistics
-    for(let item in data){
-      let country=item
-      let lastIndex=data[item].length-1
-      let population=data[item][lastIndex].population
-      let total_case=data[item][lastIndex].total_case
-      let total_death=data[item][lastIndex].total_death
-      this.latestWorldStatistics.push(
-        <div className="list-item" key={country}>
-          <div className="country">{country}</div>
-          <div className="population">{population}</div>
-          <div className="total-case">{total_case}</div>
-          <div className="total-death">{total_death}</div>
-        </div>
-      )
-    }
-    return this.latestWorldStatistics
-  }
-  componentDidMount(){
-    getData().then(data=>{
-      this.setState({data:data})
-    })
-  }
-  render(){
-    return(
-      <>
-        <div className="header">
-          <div>Country</div>
-          <div>Population</div>
-          <div>Total_case</div>
-          <div>Total_death</div>
-        </div>
-        <div className="list">
-          {Object.keys(this.state.data).length!==0?this.listAllData(this.state.data):'加载中....'}
-        </div>
-      </>
+let latestWorldStatistics: Array<JSX.Element> = []
+function listAllData(data: MergeCountryStatistics) { // 将数据组装成JSX元素列表
+  if (latestWorldStatistics.length !== 0) return latestWorldStatistics
+  for (let item in data) {
+    let country = item
+    let lastIndex = data[item].length - 1
+    let population = data[item][lastIndex].population
+    let total_case = data[item][lastIndex].total_case
+    let total_death = data[item][lastIndex].total_death
+    latestWorldStatistics.push(
+      <div className="list-item" key={country}>
+        <div className="country">{country}</div>
+        <div className="population">{population}</div>
+        <div className="total-case">{total_case}</div>
+        <div className="total-death">{total_death}</div>
+      </div>
     )
   }
+  return latestWorldStatistics
+}
+export default function List() {
+  const [data, setData] = useState<MergeCountryStatistics>({})
+  useEffect(function () {
+    // 获取新冠数据
+    getData().then(data => {
+      setData(data)
+    })
+  }, [])
+  return (
+    <>
+      <div className="header">
+        <div>Country</div>
+        <div>Population</div>
+        <div>Total_case</div>
+        <div>Total_death</div>
+      </div>
+      <div className="list">
+        {Object.keys(data).length !== 0 ? listAllData(data) : '加载中....'}
+      </div>
+    </>
+  )
 }
